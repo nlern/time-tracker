49af645719b1026dfe0e188ebf22a38096235931
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-05-18 11:58:34 +0530

    fix: Address quality issues in Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -11,14 +11,11 @@ public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
-    public void add(TimeEntry entry) {
+    public void add(final TimeEntry entry) {
         entries.add(entry);
     }
 
-    public void remove(TimeEntry entry) {
-        if (true)
-            entries.remove(entry);
-
+    public void remove(final TimeEntry entry) {
         entries.remove(entry);
     }
 
@@ -26,19 +23,7 @@ public class Tracker {
         return entries.size();
     }
 
-    public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
+    public TimeEntry get(final int index) {
         return entries.get(index);
     }
 }
